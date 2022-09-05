1c7efb60815d87e1d64a3db8a61c4166ffc8bee1
mukarrom <mukarrom.hosain.1@gmail.com>
2022-09-05 21:12:24 +0600

    navbar z-index 50 relative sticky top and marquee cut from app.js to home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import Marquee from "react-fast-marquee";
 import { Route, Routes } from "react-router-dom";
 import logo from "../src/assets/logo.jpg";
 import "./App.css";
-import MarqueeText from "./components/Home/MarqueeText";
 import Nav from "./components/Home/Nav";
 import About from "./pages/About/About";
 import AdmitForm from "./pages/AdmitForm/AdmitForm";
@@ -14,17 +12,14 @@ import Registration from "./pages/Registration/Registration";
 function App() {
   return (
     <div className="App">
-      <div className="mx-auto bg-white sticky top-0 z-50">
+      <div className="mx-auto bg-white">
         <img className="w-3/5 h-20 ml-60" src={logo} alt="" />
       </div>
       <Nav></Nav>
-      <Marquee direction="left">
-        <MarqueeText></MarqueeText>
-      </Marquee>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/admitform1" element={<AdmitForm></AdmitForm>}></Route>
+        <Route path="/admission" element={<AdmitForm></AdmitForm>}></Route>
         <Route path="about" element={<About></About>}></Route>
         <Route path="login" element={<Login></Login>}></Route>
         <Route path="regi" element={<Registration></Registration>}></Route>
--- src/components/Home/MarqueeText.js
+++ src/components/Home/MarqueeText.js
@@ -3,7 +3,7 @@ import React from "react";
 const MarqueeText = () => {
   return (
     <div className="flex">
-      <p className="text-primary mr-4">
+      <p className=" mr-4 text-lime-300">
         ***আমার সোনার বাংলা, আমি তোমায় ভালোবাসি|***
       </p>
       <p className="text-info">***আমার সোনার বাংলা, আমি তোমায় ভালোবাসি|***</p>
